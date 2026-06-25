c862c7d2957bf3ca18badbbf7454df1f5bd949e2
NusratJahanNila <nnusratjahan330@gmail.com>
2026-06-25 21:45:28 +0600

    introduction

--- Basic/introduction.js
+++ Basic/introduction.js
@@ -1,1 +1,22 @@
-// Hello 
+// Introduction 
+/**
+ * node.js is a javascript runtime environment
+ * run JS outside the browser
+ * build on Google V* JS engine
+ * Allow server-side programming using JS
+ */
+
+// Stack Flow:
+/**
+ * Frontend (UI) --> Backend(Nodejs) --> Database
+ */
+
+
+//WHY NODE.JS
+/**
+ * Single language for both frontend and backend
+ * High preformance and high scalable
+ * Non-blocking: can handle multiple request at the same time
+ * 
+ */
+
